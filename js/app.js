e06da36e2016b1658e902b1e3f089f939819ffaf
OH-MIN-JU <rjatnsdl5755@naver.com>
2025-05-09 11:45:32 +0900

    end

--- js/app.js
+++ js/app.js
@@ -9,25 +9,80 @@ const navContents = {
     'corporate',
   ],
   img: [
-    'img/thum1.jpg',
-    'img/thum2.jpg',
-    'img/thum3.jpg',
-    'img/thum4.jpg',
-    'img/thum5.jpg',
-    'img/thum6.jpg',
+    'img/thumb1.jpg',
+    'img/thumb2.jpg',
+    'img/thumb3.jpg',
+    'img/thumb4.jpg',
+    'img/thumb5.jpg',
+    'img/thumb6.jpg',
   ],
   subMenu: 'munu item',
 };
 
-// // 요소 삽입을 위한 DOM 변수
-// const nav = document.querySelector('nav ul');
+// 요소 삽입을 위한 DOM 변수
+const nav = document.querySelector('nav ul');
 
-// // 삽입 될 요소 초기화 변수
-// let navList = '';
+// 삽입 될 요소 초기화 변수
+let navList = '';
 
-// navContents.title.map((content, index) => {
-//   navList += `<li><a href="#">${content}</a></li>`;
-//   console.log(navList);
-// });
+navContents.title.map((content, index) => {
+  navList += `<li>
+                <a href="#">${content}</a>
+                <div>
+                  <h2>${content}</h2>
+                  <p><img src="${navContents.img[index]}" alt="Thumb Image" /></p>
+                  <ul>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                    <li><a href="#">${navContents.subMenu}</a></li>
+                  </ul>
+                </div>
+              </li>`;
+  // console.log(navList);
+});
 
-// nav.insertAdjacentHTML('beforeend', navList);
+nav.insertAdjacentHTML('beforeend', navList);
+
+// 200장 이미지가 삽입 될 요소
+const images = document.querySelector('.main-img');
+
+// 이미지 요소 초기화
+let imgList = '';
+
+// 200장 이미지 반복문으로 추가
+for (let i = 0; i < 200; i++) {
+  imgList += `<img src="img/pic${i}.jpg" alt="Image Frames" />`;
+  // console.log(imgList);
+}
+
+images.insertAdjacentHTML('afterbegin', imgList);
+
+// 마우스 이동 시 이미지 변환
+// 1. 좌표 계산
+// 2. 계산된 좌표를 200으로 비율 분할
+// 3. 마우스 이동 시 전체 이미지 가린다.
+// 4. 분할된 좌표 위치의 이미지를 보여준다.
+
+const x = document.querySelector('.x');
+
+window.addEventListener('mousemove', function (e) {
+  // console.log(e);
+  const clx = e.clientX; // 커서 위치
+  const screenW = document.body.offsetWidth; // 화면 사이즈
+  // Math.floor() - 소수점 날림
+  const percent = Math.floor((clx / screenW) * 200); // 200 비율로 분할된 커서 위치
+  const imgElements = document.querySelectorAll('.main-img > img');
+
+  // 마우스 이동 시 전체 이미지 숨김
+  imgElements.forEach((element) => {
+    element.style.display = 'none';
+  });
+
+  imgElements[percent].style.display = 'block';
+
+  x.textContent = percent;
+});
+
+// 900 * 1800 = 0.5
+// 0.5 * 200 = 100
